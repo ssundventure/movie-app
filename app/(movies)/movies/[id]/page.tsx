2da8a6868bec306f6278a8280892e2ba9700e55b
ssundventure <ssundventure@gmail.com>
2024-11-16 19:41:03 +0900

    dynamic metadata 추가

--- app/(movies)/movies/[id]/page.tsx
+++ app/(movies)/movies/[id]/page.tsx
@@ -1,13 +1,21 @@
 import { Suspense } from "react";
 import { API_URL } from "../../../(home)/page";
 import MovieVideos from "../../../../components/movie-videos";
-import MovieInfo from "../../../../components/movie-info";
+import MovieInfo, {getMovie} from "../../../../components/movie-info";
+
+interface IParams{
+  params: { id: string };
+}
+export async function generateMetadata({params : {id}}: IParams) {
+  const movie = await getMovie(id);
+  return {
+    title: movie.title,
+  };
+}
 
 export default async function MovieDetail({
   params: { id },
-}: {
-  params: { id: string };
-}) {
+}: IParams) {
   return (
     <div>
       <Suspense fallback={<h1>Loading movie videos</h1>}>
--- components/movie-info.tsx
+++ components/movie-info.tsx
@@ -1,7 +1,7 @@
 import { API_URL } from "../app/(home)/page";
 import infoStyles from "../styles/movie-info.module.css"
 
-async function getMovie(id: string) {
+export async function getMovie(id: string) {
   //console.log(`Fetching movies: ${Date.now()}`);
   //await new Promise((resolve) => setTimeout(resolve, 3000));
   //throw new error("Error!");
